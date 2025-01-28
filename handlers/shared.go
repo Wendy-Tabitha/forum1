656a86049906cbcc67f542f25858d1f268d0b30a
Wendy-Tabitha <wendytbt4@gmail.com>
2025-01-28 18:27:12 +0300

    fix: remove structs

--- handlers/shared.go
+++ handlers/shared.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"database/sql"
 	"html/template"
-	"time"
 )
 
 // Shared variables for the handlers package
@@ -11,32 +10,3 @@ var (
 	db        *sql.DB
 	templates *template.Template
 )
-
-// Post represents a forum post
-type Post struct {
-	ID        string
-	UserID    string
-	Title     string
-	Content   string
-	Category  string
-	CreatedAt time.Time
-	Likes     int
-	Dislikes  int
-}
-
-// User represents a forum user
-type User struct {
-	ID       string
-	Email    string
-	Username string
-	Password string
-}
-
-// Category represents a forum category
-type Category struct {
-	ID          int
-	Name        string
-	Description string
-	PostCount   int
-	Color       string
-}
